7249fc8ee72fc4c9bd3f1ef3601c8db5c1cb75fd
MartyPR <martin.p1131@gmail.com>
2023-02-01 20:32:34 -0500

    solve fix

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 const HtmlWebpackPlugin=require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
-const TerserPlugin = require('tercer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 module.exports={
